8de7cbb751fe9b8fe650af3f819cbc3decc7927c
DUCLAUX <anne-victoria.duclaux@capgemini.com>
2021-07-06 18:22:40 +0200

    add a form component to create reminder

---
+++ src/domains/reminder/services/use-delete-one.js
@@ -0,0 +1,22 @@
+import useSWR from "swr";
+// import fetch from "unfetch";
+import axios from "axios";
+
+const fetcher = url => axios.delete(url).then(res => res.data);
+
+// const fetcher = async (ressource) => {
+// 	const response = await fetch(ressource);
+// 	return await response.json();
+// };
+
+
+const useDeleteOne = (idReminder) => {
+	const { data, error } = useSWR(`http://localhost:3001/reminder/${ idReminder }`, fetcher, { method: "DELETE" });
+	return {
+		reminder: data,
+		isLoading: !error && !data,
+		isError: error
+	};
+};
+
+export default useDeleteOne;
