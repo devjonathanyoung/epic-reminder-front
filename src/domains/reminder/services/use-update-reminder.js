3f38d55074f506bed4a7ff51772159a5d1f96b28
DUCLAUX <anne-victoria.duclaux@capgemini.com>
2021-07-09 09:30:29 +0200

    create functionnality fixed

---
+++ src/domains/reminder/services/use-update-reminder.js
@@ -0,0 +1,14 @@
+import fetch from "unfetch";
+
+
+const useUpdateReminder = async(reminder) => {
+	console.log("reminder update", reminder);
+	const response = await fetch(`http://localhost:3001/reminder/${ reminder.id }`, {
+		method: "PUT",
+		headers: { "Content-Type":"application/json" },
+		body: JSON.stringify(reminder)
+	});
+	return response.json();
+};
+
+export default useUpdateReminder;
