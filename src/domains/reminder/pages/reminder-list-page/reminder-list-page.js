8948a04f6d4c3c9140483ac20f61a6348602ceb4
AnneVictoria <avduclaux@gmail.com>
2021-07-29 18:35:31 +0200

    fix/refactor states in one object

--- src/domains/reminder/pages/reminder-list-page/reminder-list-page.js
+++ src/domains/reminder/pages/reminder-list-page/reminder-list-page.js
@@ -5,33 +5,76 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import SearchBar from "../../components/searchbar/searchBar";
 
-const ReminderListPage = (props) => {
-	const [ isAsc, setIsAsc] = useState("desc");
-	const [ sortOn, setSortOn] = useState("date");
-	const [ type, setType] = useState("all");
-	const [ search, setSearch ] = useState("");
+const ReminderListPage = () => {
+	// const [ isAsc, setIsAsc] = useState("desc");
+	// const [ sortOn, setSortOn] = useState("date");
+	// const [ type, setType] = useState("all");
+	// const [ search, setSearch ] = useState("");
+
+	// const { reminders, isLoading, isError } = useReminderList(sortOn, isAsc, search, type);
 	
-	
-	const { reminders, isLoading, isError } = useReminderList(sortOn, isAsc, search, type);
+	// const handleSort = (fieldToSortOn) => {
+	// 	if (isAsc === "asc"){
+	// 		setIsAsc("desc");
+	// 	} else {
+	// 		setIsAsc("asc");
+	// 	} 
+	// 	setSortOn(fieldToSortOn);
+	// 	  };  
+
+	// const handleType = (typeToFilter) => {
+	// 	setType(typeToFilter);
+	// };
+
+	// TEST REFACTO
+
+	const [filter, setFilter] = useState({ isAsc: "desc", sortOn: "date", type: "all", search: "" });
+	const { reminders, isLoading, isError } = useReminderList(filter);
 
 const ReminderListPage = () => {
 	const { reminders, isLoading, isError } = useReminderList();
 	const [listReminders, setListReminders] = useState(reminders);
 	const { t } = useTranslation();
 	const handleSort = (fieldToSortOn) => {
-		if (isAsc === "asc"){
-			setIsAsc("desc");
+		if (filter.isAsc === "asc"){
+			setFilter((oldState) => ({ 
+				...oldState, isAsc: "desc" 
+			}));
 		} else {
-			setIsAsc("asc");
+			setFilter((oldState) => ({ 
+				...oldState, isAsc: "asc" 
+			}));
 		} 
-		setSortOn(fieldToSortOn);
-		  };  
+		setFilter((oldState) => ({ 
+			...oldState, sortOn: fieldToSortOn 
+		}));
+		  }; 
 
-	const handleType = (typeToFilter) => {
-		setType(typeToFilter);
+	const handleType = (newType) => {
+		setFilter((oldState) => ({ 
+			...oldState, type: newType 
+		}));
 	};
 
+	const setSearch = (newSearch) => {
+		setFilter( (oldState) => ({ 
+			...oldState, search: newSearch 
+		}));
+	};
+
+
 	const typesReminder = ["book", "game", "movie", "all"];
+
+	const handleReminderNotFound = () => {
+	 if (filter.type === "all") {
+			//  if (type === "all") {
+			return "No reminder yet.";
+		} else {
+			return `No reminder of type ${filter.type} yet.`;
+			// return `No reminder of type ${type} yet.`;
+
+		};
+	};
 
 	return(
 		<div>
@@ -55,7 +98,7 @@ const ReminderListPage = () => {
 				return (
 					<ReminderCard key={reminder.id} {...reminder}/>
 				);
-			})) : <h3>No reminder</h3>	}
+			})) : <h3>{handleReminderNotFound()}</h3>	}
 
 		
 		</div>
