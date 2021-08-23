12bdd9d08c90c2578aa02246c43d1232baaf283d
CamilleParpaillon <camille.parpaillon@capgemini.com>
2021-08-23 20:32:51 +0200

    change route login

--- src/domains/user/services/user-login.js
+++ src/domains/user/services/user-login.js
@@ -1,7 +1,7 @@
 import fetch from "unfetch";
 
 const userLogin = async(user) => {
-	const response = await fetch("http://localhost:3001/user/login", {
+	const response = await fetch("http://localhost:3001/auth/login", {
 		method: "POST",
 		headers: { "Content-Type":"application/json" },
 		body: JSON.stringify(user)
